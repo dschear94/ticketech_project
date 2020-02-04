c13c9fab052fdb9dcf583173138e57554febd104
derek schear <dschear94@gmail.com>
2020-02-04 11:16:10 -0500

    configuration

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+    Route,
+} from 'react-router-dom';
+
+const App = () => (
+    <div >
+        <h1>
+            hi!
+        </h1>
+    </div>
+);
+
+export default App;
